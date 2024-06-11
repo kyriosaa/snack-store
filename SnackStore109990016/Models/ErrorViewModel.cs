21de2f617af743b37f6a81b901314da6940ba122
kyriosaa <kyriosaaph@gmail.com>
2024-06-12 01:03:33 +0800

    Azure stuff

---
+++ SnackStore109990016/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SnackStore109990016.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
