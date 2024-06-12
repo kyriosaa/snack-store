42a52eeda3d3b94052eb5d855389c093a8d111c5
kyriosaa <kyriosaaph@gmail.com>
2024-06-13 00:20:32 +0800

    Added registration validators + website production build

--- API/Program.cs
+++ API/Program.cs
@@ -86,6 +86,9 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.UseCors(opt => 
 {
     opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
@@ -95,6 +98,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToController("Index", "Fallback");
 
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
